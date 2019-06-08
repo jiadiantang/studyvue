c33fb59632845ba47eb294be3a38433014758c65
沙潇 <shaxiao@shaxiaodeMac-mini.local>
2019-06-08 12:12:17 +0800

    首页

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import Vue from 'vue'
 import app from './App.vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import router from './router.js'
 
-import {Header} from 'mint-ui'
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+import {Header, Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name,Header)
-
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
+
 
 var vm= new Vue({
     el:'#app',
     render:c=>c(app),
+    router,
 
 })
--- src/router.js
+++ src/router.js
@@ -1,10 +1,22 @@
 
 import VueRouter from 'vue-router'
 
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+
+
 
 var router = new VueRouter({
     routes:[
-    ]
+        {path:'/', redirect:'/home'},
+        {path:'/home', component:HomeContainer},
+        {path:'/member', component:MemberContainer},
+        {path:'/shopcar', component:ShopcarContainer},
+        {path:'/search', component:SearchContainer},
+    ],
+    linkActiveClass:'mui-active',
 })
 
 
